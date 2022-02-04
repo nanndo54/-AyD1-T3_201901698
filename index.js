6fde46b8f7a3477cefd52ba871262e38988513fc
PabloC54 <3091865790609@ingenieria.usac.edu.gt>
2022-02-03 21:40:04 -0600

    server finished

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hola mundo 201901698</h1>')
+})
+
+app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'))
