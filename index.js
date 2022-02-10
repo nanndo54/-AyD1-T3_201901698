6e3144505fc0b9ca5e058a63c9ee3e375da1f019
pabloc54 <3091865790609@ingenieria.usac.edu.gt>
2022-02-10 10:30:35 -0600

    fixed console.log

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ app.get('/extra', (req, res) => {
   res.send('<h1>Hola mundo 201901698</h1>')
 })
 
-app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'))
+app.listen(3000, () => console.log('Servidor corriendo en  http://localhost:3000'))
